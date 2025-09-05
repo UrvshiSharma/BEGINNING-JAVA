b3bbe76897a07f4ebc86ad7c3f8d33e6c64680fe
URVSHI SHARMA <urvshisharma206@gmail.com>
2025-09-05 10:42:50 -0700

    Add files via upload

---
+++ add.java
@@ -0,0 +1,13 @@
+public class add {
+    public static void main(String args[]){
+        int a = 10;
+        int b = 25;
+        int sum = a + b;
+        int diff = a - b;
+        int mul = a * b;
+        
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(mul);
+    }
+}
